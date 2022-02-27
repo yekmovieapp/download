d16da00db008ede807d001058022d354fd3a6ab3
Yekmovie Application <99598708+yekmovieapp@users.noreply.github.com>
2022-02-27 21:36:04 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-const windowsLink = document.getElementById('windows-app').getAttribute('href');
-const androidLink = document.getElementById('android-app').getAttribute('href');
-const macLink = document.getElementById('mac-app').getAttribute('href');
-
 function handleOS() {
+    const windowsLink = document.getElementById('windows-app').getAttribute('href');
+    const androidLink = document.getElementById('android-app').getAttribute('href');
+    const macLink = document.getElementById('mac-app').getAttribute('href');
     const os = platform.os.family;
     const mainDownloadImage = document.querySelector('#main-download-inner img');
     const mainDownloadStrong = document.querySelector('#main-download-inner strong');
@@ -25,63 +24,67 @@ function handleOS() {
         mainDownloadStrong.innerHTML = 'اپلیکیشن مک';
         mainDownloadLink.setAttribute('href', macLink);
         mainDownloadLink.innerHTML = 'دانلود فایل .DMG';
+
+        document.querySelector('.display-none.text-white.position-relative.text-black').classList.remove('display-none');
     }
 }
 
 function init() {
-    handleOS();
-    const logo = document.getElementById('logo');
-    const logoPos = [logo.getBoundingClientRect().x, logo.getBoundingClientRect().y];
-    const logoClone = logo.cloneNode(true);
-    const container = document.querySelector('.container');
-    logoClone.style.position = 'absolute';
-    logoClone.style.left = (document.documentElement.clientWidth - logoClone.width) / 2 + 'px';
-    logoClone.style.top = (document.documentElement.clientHeight - logoClone.height) / 2 + 'px';
-    logoClone.id = 'logo-clone';
+    if (document.body.id == 'home') {
+        handleOS();
+        const logo = document.getElementById('logo');
+        const logoPos = [logo.getBoundingClientRect().x, logo.getBoundingClientRect().y];
+        const logoClone = logo.cloneNode(true);
+        const container = document.querySelector('.container');
+        logoClone.style.position = 'absolute';
+        logoClone.style.left = (document.documentElement.clientWidth - logoClone.width) / 2 + 'px';
+        logoClone.style.top = (document.documentElement.clientHeight - logoClone.height) / 2 + 'px';
+        logoClone.id = 'logo-clone';
 
-    document.body.appendChild(logoClone);
+        document.body.appendChild(logoClone);
 
-    const logoClonePos = [logoClone.getBoundingClientRect().x, logoClone.getBoundingClientRect().y];
-    gsap.to('#logo-clone', {
-        opacity: 0,
-        duration: .4,
-        ease: 'power1.inOut',
-        yoyo: true,
-        repeat: 3,
-        onComplete: function() {
+        const logoClonePos = [logoClone.getBoundingClientRect().x, logoClone.getBoundingClientRect().y];
+        gsap.to('#logo-clone', {
+            opacity: 0,
+            duration: .4,
+            ease: 'power1.inOut',
+            yoyo: true,
+            repeat: 3,
+            onComplete: function() {
 
-            gsap.to(logoClone, {
-                x: logoPos[0] - logoClonePos[0],
-                y: logoPos[1] - logoClonePos[1],
-                delay: .25,
-                duration: .5,
-                onComplete: function() {
-                    logo.style.opacity = 0;
-                    gsap.to(container, {
-                        opacity: 1,
-                        delay: .5,
-                        duration: .5,
-                    });
-                    gsap.set('#title-img img', {
-                        x: '101%'
-                    });
-                    gsap.to('#title-img img', {
-                        x: 0,
-                        delay: .5,
-                        duration: .5
-                    });
-                    gsap.set('#website-link-outer a', {
-                        x: '101%'
-                    });
-                    gsap.to('#website-link-outer a', {
-                        x: 0,
-                        delay: .5,
-                        duration: .5
-                    })
-                }
-            });
-        }
-    });
+                gsap.to(logoClone, {
+                    x: logoPos[0] - logoClonePos[0],
+                    y: logoPos[1] - logoClonePos[1],
+                    delay: .25,
+                    duration: .5,
+                    onComplete: function() {
+                        logo.style.opacity = 0;
+                        gsap.to(container, {
+                            opacity: 1,
+                            delay: .5,
+                            duration: .5,
+                        });
+                        gsap.set('#title-img img', {
+                            x: '101%'
+                        });
+                        gsap.to('#title-img img', {
+                            x: 0,
+                            delay: .5,
+                            duration: .5
+                        });
+                        gsap.set('#website-link-outer a', {
+                            x: '101%'
+                        });
+                        gsap.to('#website-link-outer a', {
+                            x: 0,
+                            delay: .5,
+                            duration: .5
+                        })
+                    }
+                });
+            }
+        });
+    }
 
 }
 
